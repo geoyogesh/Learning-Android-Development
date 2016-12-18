d7be2bb80de6b1f9d21fb569c37704da32b45394
Yogesh Dhanapal <geoyogesh@gmail.com>
2016-12-18 01:31:45 -0500

    added favorite toys app

---
+++ 02-FavoriteToys/app/src/main/java/com/geoyogesh/favoritetoys/MainActivity.java
@@ -0,0 +1,20 @@
+package com.geoyogesh.favoritetoys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtview = (TextView) findViewById(R.id.lstToys);
+        txtview.setText("");
+        for (String item: Toys.GetToyNames()){
+            txtview.append(item + '\n');
+        }
+    }
+}
---
+++ 02-FavoriteToys/app/src/main/java/com/geoyogesh/favoritetoys/Toys.java
@@ -0,0 +1,17 @@
+package com.geoyogesh.favoritetoys;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Bluenet on 12/18/2016.
+ */
+
+public class Toys {
+    public static ArrayList<String> GetToyNames(){
+        ArrayList<String> a1 = new ArrayList<String>();
+        a1.add("Zara");
+        a1.add("Mahnaz");
+        a1.add("Ayan");
+        return a1;
+    }
+}
